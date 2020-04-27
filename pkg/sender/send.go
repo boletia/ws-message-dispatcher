ce8064b79964fa9b999ed644034d761d6bfaf0a7
Angel Santos <angel@boletia.com>
2020-04-27 15:35:34 -0500

    slicing results dispaching to lambda

--- pkg/sender/send.go
+++ pkg/sender/send.go
@@ -1,20 +1,71 @@
 package sender
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/lambda"
+)
+
 type payloadLambdaRequest struct {
 	Message       interface{} `json:"message"`
 	ConnectionIDS []string    `json:"connection_ids"`
 }
 
+const maxRequestPerLambda = 100
+const percentageGraceSingleLambda = .2
+
+const maxRequestGraceSingleLambda = maxRequestPerLambda * percentageGraceSingleLambda
+
 // SendMessage send messages to ws-MessageSender lambda
 func (s sender) SendMessage(connections []string, msg interface{}) {
-	payload := payloadLambdaRequest{
-		Message: msg,
-	}
 
-	for i := 0; i < len(connections); i++ {
-		payload.ConnectionIDS = append(payload.ConnectionIDS, connections[i])
-		if i%50 == 0 {
-			// invoke lambda with  a go routing, using wg
+	connectionsLen := len(connections)
+
+	connectionsPerLambda := connectionsLen / maxRequestPerLambda
+	connectionsPerLambdaMod := connectionsLen % maxRequestPerLambda
+
+	suitableForSingleLambda := connectionsPerLambda <= 1 || (connectionsLen == 1 && connectionsPerLambdaMod <= maxRequestGraceSingleLambda)
+
+	if suitableForSingleLambda {
+		payload := payloadLambdaRequest{
+			Message:       msg,
+			ConnectionIDS: connections,
+		}
+		s.LambdaHandler(payload)
+	} else {
+		for idx := 0; idx < connectionsLen; idx += maxRequestPerLambda {
+
+			payload := payloadLambdaRequest{
+				Message: msg,
+			}
+
+			sliceEnd := idx + maxRequestPerLambda
+
+			if sliceEnd > connectionsLen {
+				payload.ConnectionIDS = connections[idx:connectionsLen]
+			} else {
+				payload.ConnectionIDS = connections[idx:sliceEnd]
+			}
+			go s.LambdaHandler(payload)
 		}
 	}
+
+}
+
+func (s sender) LambdaHandler(payload payloadLambdaRequest) {
+
+	payloadJSON, err := json.Marshal(payload)
+
+	if err != nil {
+		fmt.Println("Json Marshalling error")
+	}
+
+	input := &lambda.InvokeInput{
+		FunctionName: s.lambdaName,
+		Payload:      payloadJSON,
+	}
+
+	s.Invoke(input)
+
 }
